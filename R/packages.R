42f42317c33fdf904062d838f1b37a3a73138e43
NickNehm98 <nicknehmdesign@gmail.com>
2024-12-22 16:39:45 +0100

    updated doc

--- R/packages.R
+++ R/packages.R
@@ -3,6 +3,7 @@
 #' This code checks for the installation of required packages and installs any missing ones.
 #' It then loads all the specified packages into the R session.
 #'
+#' @name install_load_packages
 #' @param packages A character vector of package names to check, install, and load.
 #' @details If a package is not installed, it will be installed with all its dependencies.
 #' Messages will indicate any missing packages that are being installed.
